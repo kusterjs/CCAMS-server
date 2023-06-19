390f1808f1792d0c3fa18c7d29b7954c58255346
Jonas Kuster <jonas@kusternet.ch>
2023-06-19 23:49:37 +0200

    file name changed cleaning of user cache added

--- clean.php
+++ clean.php
@@ -5,5 +5,7 @@ include_once('CCAMS.php');
 if (array_key_exists('debug',$_GET)) $CCAMS = new CCAMS('/../cron/bin/',true);
 else $CCAMS = new CCAMS('/../cron/bin/');
 $CCAMS->clean_squawk_cache();
+$CCAMS->clean_user_cache();
+
 
 ?>
